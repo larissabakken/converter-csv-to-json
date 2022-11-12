f985fa151e3a418e1001e1e65fb076b0a0c5be2d
Larissa Ramos <devlarissasr@gmail.com>
2022-11-11 21:02:43 -0300

    generator csv

---
+++ generator-csv.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require ('path');
+const crypto = require('crypto');
+
+const csvFile = fs.createWriteStream(
+    path.join(__dirname, 'big-file.csv')
+);
+
+csvFile.write('id; name; email; password\n');
+
+for (let i = 0; i < 100; i++) {
+    const id = i;
+    const name = crypto.randomBytes(10).toString('hex');
+    const email = crypto.randomBytes(10).toString('hex') + '@gmail.com';
+    const password = crypto.randomBytes(10).toString('hex');
+    csvFile.write(`${id};${name};${email};${password}\n`);
+}
+csvFile.end();
+
+
+
+
